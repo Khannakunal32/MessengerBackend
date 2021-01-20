238a0331ba81cbf84f716c8d0bfff372aeb2f004
Aakash bhardwaj <maxakash1999@gmail.com>
2021-01-20 22:42:41 +0530

    working/backend-initiated

---
+++ api/users/user.service.js
@@ -0,0 +1,90 @@
+const pool = require("../../config/database");
+
+module.exports = {
+  create: (data, callBack) => {
+    pool.query(
+      `insert into users(firstName, lastName, email, password) 
+                values(?,?,?,?)`,
+      [
+        data.firstName,
+        data.lastName,
+        data.email,
+        data.password
+      ],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
+  getUserByUserEmail: (email, callBack) => {
+    pool.query(
+      `select * from users where email = ?`,
+      [email],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0],);
+      }
+    );
+  },
+  getUserByUserId: (id, callBack) => {
+    pool.query(
+      `select id,firstName,lastName,email from users where id = ?`,
+      [id],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  getUsers: callBack => {
+    pool.query(
+      `select id,firstName,lastName,email from users`,
+      [],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
+  updateUser: (data, callBack) => {
+    pool.query(
+      `update users set firstName=?, lastName=?, gender=?, email=?, password=?, number=? where id = ?`,
+      [
+        data.first_name,
+        data.last_name,
+        data.gender,
+        data.email,
+        data.password,
+        data.number,
+        data.id
+      ],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  deleteUser: (data, callBack) => {
+    pool.query(
+      `delete from registration where id = ?`,
+      [data.id],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  }
+};
---
+++ config/database.js
@@ -0,0 +1,13 @@
+const { createPool } = require("mysql");
+
+const pool = createPool({
+  port:process.env.MYSQL_PORT,
+  host: process.env.MYSQL_HOST,
+  database: process.env.MYSQL_DATABASE,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  
+  connectionLimit: 10
+});
+
+module.exports = pool;
